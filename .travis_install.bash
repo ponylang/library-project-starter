979e2c535636741012940f5434d57f240fe37a14
Eric Pires <eric.pires@devtecnologia.com.br>
2017-10-31 08:56:58 -0200

    Fix Travis fail for libraries using regex

--- .travis_install.bash
+++ .travis_install.bash
@@ -42,6 +42,7 @@ then
   popd
 else
   echo -e "\033[0;32mInstalling latest ponyc release\033[0m"
+  download_pcre
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "D401AB61 DBE1D0A2"
   echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
   sudo apt-get update
