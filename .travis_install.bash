49d275d2754c11ed1da6c8a5b6ca4bb9430aedfb
Sean T Allen <sean@monkeysnatchbanana.com>
2017-10-31 13:28:49 -0400

    Switch it up

--- .travis_install.bash
+++ .travis_install.bash
@@ -41,8 +41,9 @@ then
   install-ponyc-master
   popd
 else
-  echo -e "\033[0;32mInstalling latest ponyc release\033[0m"
+  echo "Installing ponyc runtime dependencies..."
   download_pcre
+  echo -e "\033[0;32mInstalling latest ponyc release\033[0m"
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "D401AB61 DBE1D0A2"
   echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
   sudo apt-get update
